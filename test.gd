54430d863e7f6553ad7493dd393bd8406101a9e8
ItsAleph <igorekkrupskij@gmail.com>
2023-04-05 14:47:25 +0000

    More support for anotations (annotated code still doesnt work

--- test.gd
+++ test.gd
@@ -1,3 +1,29 @@
-# Hello?
-@hello()
-@hello
+# Test variable declaration
+var num = 12
+
+# Test typed variable declation
+var num: int = 12
+
+# Test minimal annotation
+@onready
+
+# Test parentised annotation
+@onready()
+
+# Test annotation with one arg
+@onready("arg")
+
+# Test annotation with several args
+@onready("arg1", "arg2")
+
+# Test annotation with one arg and trailing comma
+@onready("arg",)
+
+# Test annotation with several args and trailing comma
+@onready("arg1", "arg2",)
+
+# Test minimal annotation with text after it
+@onready var number = 1
+
+# Test parentised annotation with text after it
+@onready() var number = 1
