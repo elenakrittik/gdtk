303f4a86e2647069fc24fa84939918138ccc9ebd
elenakrittik <dev.elenakrittik@gmail.com>
2024-04-14 10:49:25 +0300

    feat(gdtk-cli): Allow specifying `-` in `gdtk dev [parse|lex]` commands to read from stdin.

--- crates/gdtk/src/commands/dev/lex.rs
+++ crates/gdtk/src/commands/dev/lex.rs
@@ -2,8 +2,10 @@ use std::path::PathBuf;
 
 use itertools::Itertools;
 
+use crate::commands::dev::get_content;
+
 pub fn run(file: PathBuf) -> anyhow::Result<()> {
-    let content = std::fs::read_to_string(file)?;
+    let content = get_content(file)?;
     let lexed = gdtk_lexer::lex(&content);
 
     eprintln!("Lexer output:\n```ron\n{:#?}\n```", &lexed.collect_vec());
--- crates/gdtk/src/commands/dev/parse.rs
+++ crates/gdtk/src/commands/dev/parse.rs
@@ -1,7 +1,9 @@
 use std::path::PathBuf;
 
+use crate::commands::dev::get_content;
+
 pub fn run(file: PathBuf) -> anyhow::Result<()> {
-    let content = std::fs::read_to_string(file)?;
+    let content = get_content(file)?;
     let lexed = gdtk_lexer::lex(&content);
     let parsed = gdtk_parser::parse_file(lexed)?;
 
