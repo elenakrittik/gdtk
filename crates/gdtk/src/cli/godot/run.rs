f55bb6c740c4991248669ca91d0571e3f701eeca
elenakrittik <dev.elenakrittik@gmail.com>
2024-12-29 22:47:18 +0300

    update

--- crates/gdtk/src/cli/godot/run.rs
+++ crates/gdtk/src/cli/godot/run.rs
@@ -1,28 +1,49 @@
-use std::process::Command;
+use std::process::{Command, Stdio};
 
 use gdtk_gvm::VersionManager;
 
-use crate::cli::utils::prompt_local_version;
+use crate::cli::{unknown, utils::prompt_local_version};
 
-pub struct GodotRunCommand;
+pub struct GodotRunCommand {
+    extra_args: Vec<String>,
+}
 
 impl tapcli::Command for GodotRunCommand {
     type Error = anyhow::Error;
 
-    fn parse(_: &mut tapcli::Parser) -> Result<Self, Self::Error> {
-        Ok(Self)
+    fn parse(parser: &mut tapcli::Parser) -> Result<Self, Self::Error> {
+        let mut extra_args = Vec::new();
+
+        for arg in parser {
+            match arg {
+                tapcli::Arg::Value(value) => extra_args.push(value),
+                _ => unknown!(arg),
+            }
+        }
+
+        Ok(Self { extra_args })
     }
 
     fn run(self) -> Result<Self::Output, Self::Error> {
         let manager = VersionManager::load()?;
         let version = prompt_local_version(&manager)?;
 
-        let Some(version) = manager.get_version(&version.name, version.mono) else {
-            eprintln!("Godot {} is not installed.", version);
-            return Ok(());
-        };
+        let program = version.path().join("godot");
 
-        let mut child = Command::new(version.path().join("godot")).spawn()?;
+        eprintln!(
+            "Running `{}{}`",
+            program,
+            self.extra_args.iter().fold(String::new(), |mut acc, arg| {
+                acc.push(' ');
+                acc.push_str(arg.as_str());
+                acc
+            })
+        );
+
+        let mut child = Command::new(program)
+            .args(self.extra_args)
+            .stdout(Stdio::null())
+            .spawn()?;
 
         child.wait()?;
 
